6577409e3a9bc51ae8eeb85ca1c7e236eeb86120
tomrzonta <tomrzonta@gmail.com>
2025-02-11 21:36:27 -0300

    trackeamento

--- TESTE_GIT.R
+++ TESTE_GIT.R
@@ -1,3 +1,7 @@
 testar
 install.packages("renv")
 library(renv)
+renv::init()
+renv::snapshot() # COMANDO PARA O TRACKEAMENTO DO QUE FOI FEITO
+                 # COMANDO PARA BAIXAR O QUE FOI FEITO NO OUTRO USUÁRIO renv::restore()
+
